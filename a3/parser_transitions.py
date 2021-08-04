9ba6d819718759907ef3d7f6ec075c8ba324403a
leinaxd <leinaxd@gmail.com>
2021-08-04 05:35:17 +0000

    a3: 2.e fixed en parser_transitions

--- a3/parser_transitions.py
+++ a3/parser_transitions.py
@@ -55,11 +55,11 @@ class PartialParse(object):
         ###         3. Right Arc
         if transition == "S":
             self.stack.append( self.buffer.pop(0) )
-        elif transition == "LA" and len(self.stack)>=2:
+        elif transition == "LA":
             wi = self.stack.pop(-2)
             wj = self.stack[-1]
             self.dependencies.append( (wj, wi) )
-        elif transition == "RA" and len(self.stack)>=2:
+        elif transition == "RA":
             wi = self.stack[-2]
             wj = self.stack.pop(-1)
             self.dependencies.append((wi, wj))
@@ -112,18 +112,19 @@ def minibatch_parse(sentences, model, batch_size):
     ###             contains references to the same objects. Thus, you should NOT use the `del` operator
     ###             to remove objects from the `unfinished_parses` list. This will free the underlying memory that
     ###             is being accessed by `partial_parses` and may cause your code to crash.
+    #Los dependecies no se pueden desordenar!
     partial_parses = [PartialParse(sentence) for sentence in sentences]
     unfinished_parses = partial_parses.copy()
 
     while unfinished_parses:
         batchSize = min(batch_size, len(unfinished_parses))
-        batch = unfinished_parses[0:batchSize]
-        transitions = model.predict(batch)
-        for i in range(batchSize):
-            batch[i].parse_step( transitions[i] )
-            if len(batch[i].stack) == 1 and batch[i].buffer == []:
-                dependencies.append(batch[i].dependencies)
-                unfinished_parses.remove(batch[i])
+        mini_batch = unfinished_parses[0:batchSize]
+        transitions = model.predict(mini_batch)
+        for parse, transition in zip(mini_batch, transitions):
+            parse.parse_step( transition )
+            if len(parse.stack) == 1 and parse.buffer == []:
+                unfinished_parses.remove(parse)
+    dependencies = [parse.dependencies for parse in partial_parses]
     ### END YOUR CODE
 
     return dependencies
