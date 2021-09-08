0193c7d827c4c8d9466e082c7459c44de7fab34f
leinaxd <leinaxd@gmail.com>
2021-09-08 15:43:00 +0000

    a4 1e: nmt decoder

--- a4/nmt_model.py
+++ a4/nmt_model.py
@@ -249,12 +249,14 @@ class NMT(nn.Module):
         ###         https://pytorch.org/docs/stable/torch.html#torch.cat
         ###     Tensor Stacking:
         ###         https://pytorch.org/docs/stable/torch.html#torch.stack
-
-
-
-
-
-
+        enc_hiddens_proj = self.att_projection(enc_hiddens)                     #Pre calculo W_att · h_i
+        Y = self.model_embeddings.target(target_padded)
+        for Y_t in Y:
+            Ybar_t = torch.cat((Y_t, o_prev), -1)
+            dec_state, o_t, e_t = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            combined_outputs.append(o_t)
+            o_prev = o_t
+        combined_outputs = torch.stack(combined_outputs)
         ### END YOUR CODE
 
         return combined_outputs
